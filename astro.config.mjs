e1cdc4d75d0b020984f1e1f0fc8b233cc160a4da
luca0898 <lucaguiraldello@hotmail.com>
2026-01-01 19:16:18 -0300

    feat: custom domain configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://luca0898.github.io',
-  base: '/portifolio',
+  site: 'https://lucaguiraldello.net',
   integrations: [tailwind()],
 });
